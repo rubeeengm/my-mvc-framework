21788adc57d319732021d452d3fb8d2706dbfe23
rubeeengm <rubeeengm@outlook.com>
2020-11-16 20:02:07 -0600

    + create aplication and router + enable autoloading using composer

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
 
-echo "Hello world";
+require_once __DIR__.'/vendor/autoload.php';
+
+use app\core\Application;
+
+$app = new Application();
+
+$app->router->get('/', function() {
+	return 'Hello world';
+});
+
+$app->run();
